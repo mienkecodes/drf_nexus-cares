75fb6ab623cd1a4e933a8f2c819f59d6ea1efdc5
Mienke Dreyer <mienke.hattingh@gmail.com>
2023-08-07 13:20:09 +1000

    added fields to customer database -  not working

---
+++ crowdfunding/users/__init__.py
@@ -0,0 +1,20 @@
+# crowdfunding/users/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    phone_number = forms.CharField(max_length=15, required=False)
+
+    AVAILABILITY_CHOICES = (
+        ('available', 'Available'),
+        ('not_available', 'Not Available'),
+        ('busy', 'Busy'),
+        ('away', 'Away'),
+        # Add more choices as needed
+    )
+    availability = forms.ChoiceField(choices=AVAILABILITY_CHOICES)
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('phone_number', 'availability')
--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -1,9 +1,29 @@
 
 
-# Create your models here.
+# crowdfunding/users/models.py
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
 class CustomUser(AbstractUser):
-    pass
+    skills = models.CharField(max_length=15, blank=True, null=True)
+    
+    AVAILABILITY_CHOICES = (
+        ('flexible', 'flexible'),
+        ('evenings', 'evenings'),
+        ('mornings', 'mornings'),
+        ('weekends', 'weekends'),
+        ('not_currently_available', 'currently unavailable but pop me on the list'),
+        ('busy', 'busy'),
+        ('unavailable', 'unavailable'),
+
+        # Add more choices as needed
+    )
+
+    availability = models.CharField(max_length=200, choices=AVAILABILITY_CHOICES)
+
+
+    # pass ???
     def __str__(self):
         return self.username
 
